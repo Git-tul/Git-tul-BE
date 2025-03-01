f5cc7493f1908a8bfb4a65ff2cb8cafa4eacaca8
Soap <miensop51@gmail.com>
2025-03-01 10:14:28 +0900

    chore. kotlin -> java

---
+++ src/test/java/io/gittul/gittulbe/GitTulBeApplicationTests.java
@@ -0,0 +1,12 @@
+package io.gittul.gittulbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTulBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
