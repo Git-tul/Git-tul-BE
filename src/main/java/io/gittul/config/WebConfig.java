b9964e5414d31481b33d7c1a4d9707600038c54d
Soap <miensop51@gmail.com>
2025-03-05 22:38:24 +0900

    feat. N + 1 쿼리 발견 위한 Inspector 설정

--- src/main/java/io/gittul/config/WebConfig.java
+++ src/main/java/io/gittul/config/WebConfig.java
@@ -1,23 +1,28 @@
 package io.gittul.config;
 
 import io.gittul.infra.auth.aop.AuthenticationArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthenticationArgumentResolver authenticationArgumentResolver;
-
-    public WebConfig(AuthenticationArgumentResolver authenticationArgumentResolver) {
-        this.authenticationArgumentResolver = authenticationArgumentResolver;
-    }
+    private final LoggingInterceptor loggingInterceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authenticationArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
     }
 }
