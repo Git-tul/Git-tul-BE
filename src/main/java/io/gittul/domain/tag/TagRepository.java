bbb52d72a28ba322ce2f166ff5bcf8e09a235f6d
Soap <miensop51@gmail.com>
2025-03-05 09:13:00 +0900

    feat. 태그 생성 메서드

--- src/main/java/io/gittul/domain/tag/TagRepository.java
+++ src/main/java/io/gittul/domain/tag/TagRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    List<Tag> findAllByTagName(String tagName);
+    Set<Tag> findAllByTagNameIn(List<String> tagNames);
 }
--- src/main/java/io/gittul/domain/tag/entity/Tag.java
+++ src/main/java/io/gittul/domain/tag/entity/Tag.java
@@ -1,7 +1,5 @@
 package io.gittul.domain.tag.entity;
 
-import io.gittul.domain.post.entity.Post;
-import io.gittul.domain.user.entity.User;
 import io.gittul.global.jpa.EntityTimeStamp;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -32,27 +30,9 @@ public class Tag extends EntityTimeStamp {
     @OneToMany(mappedBy = "tag", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UserInterest> interests = new ArrayList<>();
 
-
-    // Todo. public ?
-    protected void addToPost(Post post) {
-        PostTag postTag = new PostTag(post, this);
-        postTags.add(postTag);
-        post.getPostTags().add(postTag);
-    }
-
-    protected void removeFromPost(Post post) {
-        postTags.removeIf(postTag -> postTag.getPost().equals(post));
-        post.getPostTags().removeIf(postTag -> postTag.getTag().equals(this));
-    }
-
-    protected void addToInterest(User user) {
-        UserInterest interest = new UserInterest(user, this);
-        interests.add(interest);
-        user.getDetails().getInterests().add(interest);
-    }
-
-    protected void removeFromInterest(User user) {
-        interests.removeIf(interest -> interest.getUser().equals(user));
-        user.getDetails().getInterests().removeIf(interest -> interest.getTag().equals(this));
+    public static Tag of(String tagName) {
+        Tag tag = new Tag();
+        tag.tagName = tagName;
+        return tag;
     }
 }
