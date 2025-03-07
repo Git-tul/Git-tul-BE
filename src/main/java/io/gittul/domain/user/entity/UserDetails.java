1a0f3c140e50c041a935541a9e70ed3f1ff3cb7a
Soap <miensop51@gmail.com>
2025-03-07 20:29:34 +0900

    feat. 관계 필드의 fetch type 수정

--- src/main/java/io/gittul/domain/user/entity/UserDetails.java
+++ src/main/java/io/gittul/domain/user/entity/UserDetails.java
@@ -1,14 +1,15 @@
 package io.gittul.domain.user.entity;
 
-import io.gittul.domain.follow.entity.UserFollow;
 import io.gittul.domain.bookmark.entity.Bookmark;
 import io.gittul.domain.comment.entity.Comment;
+import io.gittul.domain.follow.entity.UserFollow;
 import io.gittul.domain.like.entity.UserLikeComment;
 import io.gittul.domain.like.entity.UserLikePost;
 import io.gittul.domain.post.entity.Post;
 import io.gittul.domain.tag.entity.UserInterest;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -24,27 +25,27 @@ import java.util.Set;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserDetails {
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Post> posts = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Comment> comments = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private Set<UserLikePost> likedPosts = new HashSet<>();
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private Set<UserLikeComment> likedComments = new HashSet<>();
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Bookmark> bookmarks = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<UserInterest> interests = new ArrayList<>();
 
-    @OneToMany(mappedBy = "follower", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "follower", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private Set<UserFollow> followings = new HashSet<>();
 
-    @OneToMany(mappedBy = "followee")
+    @OneToMany(mappedBy = "followee", fetch = FetchType.LAZY)
     private Set<UserFollow> followers = new HashSet<>();
 }
