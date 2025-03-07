43eaf0268fa370144bc4cd3d0ed5b474147ee301
soap <miensop51@gmail.com>
2025-03-07 20:00:27 +0900

    feat. 글 조회시 최신순

--- src/main/java/io/gittul/domain/post/PostRepository.java
+++ src/main/java/io/gittul/domain/post/PostRepository.java
@@ -8,5 +8,7 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findAllByUserUserIdIn(List<Long> userIds);
+    List<Post> findAllByUserUserIdInOrderByCreatedAtDesc(List<Long> userIds);
+
+    List<Post> findAllByOrderByCreatedAtDesc();
 }
