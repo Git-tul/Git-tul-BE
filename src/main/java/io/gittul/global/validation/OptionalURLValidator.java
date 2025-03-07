689dae7adf66d09934356cecd26d17b16841add5
Soap <miensop51@gmail.com>
2025-03-07 19:49:20 +0900

    feat. 선택적 URL validation 어노테이션

---
+++ src/main/java/io/gittul/global/validation/OptionalURLValidator.java
@@ -0,0 +1,25 @@
+package io.gittul.global.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class OptionalURLValidator implements ConstraintValidator<OptionalURL, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) return true;
+
+        try {
+            new URL(value);
+            return true;
+        } catch (MalformedURLException e) {
+            context.buildConstraintViolationWithTemplate(context.getDefaultConstraintMessageTemplate())
+                    .addConstraintViolation();
+            return false;
+        }
+    }
+}
