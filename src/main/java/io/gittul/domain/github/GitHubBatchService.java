6ba346b33b292091b823d023eeecd48a0b0e35c0
Soap <miensop51@gmail.com>
2025-03-09 09:10:26 +0900

    feat. 트렌딩 요약 배치 개선
    병럴 처리
    로깅 추가
    시간대 수정

--- src/main/java/io/gittul/domain/github/GitHubBatchService.java
+++ src/main/java/io/gittul/domain/github/GitHubBatchService.java
@@ -1,12 +1,17 @@
 package io.gittul.domain.github;
 
+import io.gittul.domain.post.dto.PostFeedResponse;
 import io.gittul.domain.user.UserInquiryService;
 import io.gittul.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class GitHubBatchService {
@@ -19,6 +24,8 @@ public class GitHubBatchService {
     @Scheduled(cron = "0 30 0 * * ?")
     public void scheduleDailyTrendingRepositoriesSummery() {
         User admin = userInquiryService.getUserById(adminId);
-        githubService.getDailyTrendingRepositoriesSummery(admin);
+        List<PostFeedResponse> responses = githubService.getDailyTrendingRepositoriesSummery(admin);
+
+        log.info("[요약 생성] {} 개의 트렌딩 레포지토리 요약이 생성되었습니다.", responses.size());
     }
 }
--- src/main/java/io/gittul/domain/github/GithubService.java
+++ src/main/java/io/gittul/domain/github/GithubService.java
@@ -4,6 +4,7 @@ import io.gittul.domain.github.api.GitHubApiService;
 import io.gittul.domain.github.api.GithubTrendingApiService;
 import io.gittul.domain.github.api.dto.RepositoryBasicInfoResponse;
 import io.gittul.domain.github.api.dto.RepositoryInfo;
+import io.gittul.domain.github.api.dto.TrendingRepositoryApiResponse;
 import io.gittul.domain.github.entity.GitHubRepository;
 import io.gittul.domain.post.PostService;
 import io.gittul.domain.post.dto.PostFeedResponse;
@@ -13,10 +14,12 @@ import io.gittul.infra.ai.summery.dto.RepositorySummary;
 import io.gittul.infra.ai.summery.dto.SummaryAndRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 @RequiredArgsConstructor
@@ -29,27 +32,24 @@ public class GithubService {
     private final SummaryService summaryService;
     private final PostService postService;
 
-
     public List<PostFeedResponse> getDailyTrendingRepositoriesSummery(User admin) {
-        // Todo. 병렬처리 , 이미 등록된 레포 제외?
-        List<SummaryAndRepository> repositories = trendingApiService.getDailyTrendingRepositories().stream()
-                .map(repo -> apiService.getRepositoryInfo(repo.author(), repo.name()))
-                .map(Mono::block).filter(Objects::nonNull)
-                .map(info -> {
-                    GitHubRepository repository = getOrCreateRepository(info);
-                    RepositorySummary summary = summaryService.generateSummary(info);
+        List<TrendingRepositoryApiResponse> trendingRepositories = trendingApiService.getDailyTrendingRepositories();
 
+        List<SummaryAndRepository> newRepositories = getNewRepositoryInfos(trendingRepositories).parallelStream()
+                .map(info -> {
+                    GitHubRepository repository = saveRepository(info);
+                    RepositorySummary summary = summaryService.generateSummary(info);
                     return new SummaryAndRepository(summary, repository);
                 })
                 .toList();
 
-
-        return repositories.stream()
+        return newRepositories.stream() // Note. 병렬처리시 tag getOrSave 에서 유니크 제약 조건 에러 발생 가능성
                 .map(repo -> postService.createPostFromSummary(repo.repository(), repo.summary(), admin))
                 .toList();
     }
 
-    private GitHubRepository getOrCreateRepository(RepositoryInfo info) {
+
+    public GitHubRepository saveRepository(RepositoryInfo info) {
         RepositoryBasicInfoResponse basicInfo = info.basicInfo();
 
         return repository.findByRepoUrl(basicInfo.url())
@@ -60,5 +60,27 @@ public class GithubService {
                                 basicInfo.stars(),
                                 basicInfo.forks()
                         )));
+    }
+
+    // Todo. 시간 비교 필요
+    private List<RepositoryInfo> getNewRepositoryInfos(List<TrendingRepositoryApiResponse> trendingRepositories) {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        List<CompletableFuture<RepositoryInfo>> futures = trendingRepositories.stream()
+                .map(repo -> CompletableFuture.supplyAsync(() ->
+                        apiService.getRepositoryInfo(repo.author(), repo.name()).block(), executor))
+                .toList();
+
+        List<RepositoryInfo> result = futures.stream()
+                .map(CompletableFuture::join)
+                .filter(Objects::nonNull)
+                .filter(this::isNew)
+                .toList();
+
+        executor.shutdown();
+        return result;
+    }
+
+    private boolean isNew(RepositoryInfo info) {
+        return repository.findByRepoUrl(info.basicInfo().url()).isEmpty();
     }
 }
