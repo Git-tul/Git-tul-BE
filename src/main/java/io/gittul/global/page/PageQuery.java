beba242c0b6ab2788543bc94ba044a0d046167e5
Soap <miensop51@gmail.com>
2025-03-09 14:10:30 +0900

    fix. 페이지 쿼리 입력하지 않았을 때 처리

--- src/main/java/io/gittul/global/page/PageQuery.java
+++ src/main/java/io/gittul/global/page/PageQuery.java
@@ -3,11 +3,13 @@ package io.gittul.global.page;
 import org.springframework.data.domain.PageRequest;
 
 public record PageQuery(
-        int page,
-        int size
+        Integer page,
+        Integer size
 ) {
 
     public PageRequest toRequest() {
-        return PageRequest.of(page, size);
+        return PageRequest.of(
+                page == null ? 0 : page,
+                size == null ? 10 : size);
     }
 }
