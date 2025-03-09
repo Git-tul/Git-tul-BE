2a24ef1c620380475c0c96ef95a1846a4af3a1ca
Soap <miensop51@gmail.com>
2025-03-09 15:38:54 +0900

    docs. swagger 설정

---
+++ src/main/java/io/gittul/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package io.gittul.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(info());
+    }
+
+    private Info info() {
+        return new Info()
+                .title("깃털 API")
+                .description("깃털 API 문서")
+                .version("0.1");
+    }
+}
