dc5642cff7d3fd2feb833e1465972d7121e59fd9
Soap <miensop51@gmail.com>
2025-03-19 07:54:22 +0900

    refactor. slack 관련 이동
    (외부 서비스 모듈 분리 고려)

---
+++ app/src/main/kotlin/io/gittul/app/infra/notify/Color.kt
@@ -0,0 +1,10 @@
+package io.gittul.app.infra.notify
+
+enum class Color(val code: String) {
+    GREEN("#36a64f"),
+    RED("#ff0000"),
+    BLUE("#0000ff"),
+    YELLOW("#ffff00"),
+    BLACK("#000000"),
+    WHITE("#ffffff");
+}
