d7f68a5e83b217f92a514007f7de8adb13a70823
Soap <miensop51@gmail.com>
2025-03-26 22:15:33 +0900

    refactor. 모듈별 의존성 분리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,9 +4,24 @@ plugins {
 }
 
 dependencies {
+    // kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+    // jwt
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+
+    // api
+    implementation("com.slack.api:slack-api-client:1.45.3")
+    implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter:1.0.0-SNAPSHOT")
+
+    // swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
 }
 
 kotlin {
