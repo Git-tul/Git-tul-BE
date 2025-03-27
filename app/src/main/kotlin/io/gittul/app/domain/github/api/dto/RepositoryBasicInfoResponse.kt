5b5518be2dbb224ebaefea953eb31655469c3dd0
Soap <miensop51@gmail.com>
2025-03-27 18:00:37 +0900

    fix. API 스펙과 일치하지 않는 DTO 필드 수정
    OpenAI 프롬프트에 전달하기 위해 Json 으로 변환

--- app/src/main/kotlin/io/gittul/app/domain/github/api/dto/RepositoryBasicInfoResponse.kt
+++ app/src/main/kotlin/io/gittul/app/domain/github/api/dto/RepositoryBasicInfoResponse.kt
@@ -1,20 +1,40 @@
 package io.gittul.app.domain.github.api.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 data class RepositoryBasicInfoResponse(
-    val author: String,
     val name: String,
-    val avatar: String,
-    val description: String,
+    val owner: Owner,
+    val description: String?,
 
     @JsonProperty("html_url")
     val url: String,
-    val language: String,
+
+    val language: String?,
 
     @JsonProperty("stargazers_count")
     val stars: Int,
 
     @JsonProperty("forks_count")
     val forks: Int
-)
+) {
+    override fun toString(): String { // OpenAI 프롬프트에 전달하기 위해 Json 으로 변환
+        return jacksonObjectMapper().writeValueAsString(this);
+    }
+}
+
+data class Owner(
+
+    @JsonProperty("login")
+    val author: String,
+
+    @JsonProperty("avatar_url")
+    val avatar: String
+) {
+    override fun toString(): String {
+        return jacksonObjectMapper().writeValueAsString(this);
+    }
+}
