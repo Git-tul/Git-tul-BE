53eff39b403a84901adb53e40dcb81740a10ecdc
Soap <miensop51@gmail.com>
2025-03-27 18:01:53 +0900

    feat. 요약 스케줄 활성화

--- app/src/main/kotlin/io/gittul/app/AppApplication.kt
+++ app/src/main/kotlin/io/gittul/app/AppApplication.kt
@@ -2,7 +2,9 @@ package io.gittul.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class AppApplication
 
