a344b8896964f3d6bc202270ba99ca8e227157a6
Soap <miensop51@gmail.com>
2025-03-29 17:00:24 +0900

    refactor. Bean 등록 대신 entityscan 으로 수정

--- app/src/main/kotlin/io/gittul/app/AppApplication.kt
+++ app/src/main/kotlin/io/gittul/app/AppApplication.kt
@@ -2,9 +2,11 @@ package io.gittul.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableScheduling
+@ComponentScan(basePackages = ["io.gittul.core", "io.gittul.infra", "io.gittul.app"])
 @SpringBootApplication
 class AppApplication
 
