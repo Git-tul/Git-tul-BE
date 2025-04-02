99e61fbc0becdefd558dde960310f8779ac30437
Soap <miensop51@gmail.com>
2025-04-03 02:08:50 +0900

    feat. 로깅 개선을 위한 레포지토리 fullname getter 추가

--- infra/src/main/kotlin/io/gittul/infra/github/dto/RepositoryInfo.kt
+++ infra/src/main/kotlin/io/gittul/infra/github/dto/RepositoryInfo.kt
@@ -3,4 +3,7 @@ package io.gittul.infra.github.dto
 data class RepositoryInfo(
     val basicInfo: RepositoryBasicInfoResponse,
     val readme: String?
-)
+) {
+    val fullName: String
+        get() = "${basicInfo.owner}/${basicInfo.name}"
+}
